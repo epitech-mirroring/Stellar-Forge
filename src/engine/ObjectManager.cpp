635621e6f2b88f7f74e1a2c6df3244c75a616891
Aubane <aubane.nourry@epitech.eu>
2024-09-24 23:11:32 +0200

    :sparkles: Add Object and Scene manager classes implementation

---
+++ src/engine/ObjectManager.cpp
@@ -0,0 +1,101 @@
+/*
+** EPITECH PROJECT, 2024
+** StellarForge
+** File description:
+** ObjectManager.cpp
+*/
+
+#include "ObjectManager.hpp"
+
+std::unordered_map<long, void*> ObjectManager::getObjects() const
+{
+    return _objects;
+}
+
+size_t ObjectManager::getObjectCount() const
+{
+    return _objects.size();
+}
+
+bool ObjectManager::objectExists(const long& id) const
+{
+    return _objects.find(id) != _objects.end();
+}
+
+/*bool ObjectManager::objectExists(void* object) const
+{
+    for (const auto& obj : _objects) {
+        if (obj.second == object) {
+            return true;
+        }
+    }
+    return false;
+}*/
+
+void ObjectManager::addObject(const long& id, void* object)
+{
+    if (_objects.find(id) != _objects.end()) {
+        //raise Warning Object with ID id already exists: overwriting
+    }
+    _objects[id] = object;
+}
+
+void ObjectManager::addObjects(const std::unordered_map<long, void*>& objects)
+{
+    for (const auto& object : objects) {
+        addObject(object.first, object.second);
+    }
+}
+
+void ObjectManager::removeObject(const long& id)
+{
+    if (_objects.erase(id) == 0) {
+        //raise Warning Object with ID id does not exist
+    }
+}
+
+void ObjectManager::removeObjects(const std::vector<long>& ids)
+{
+    for (const auto& id : ids) {
+        removeObject(id);
+    }
+}
+
+void ObjectManager::updateObject(const long& id, void* updatedObject)
+{
+    if (!objectExists(id)) {
+        //raise Warning Object does not exist, creating a new
+    }
+    _objects[id] = updatedObject;
+}
+
+void* ObjectManager::getObjectById(const long& id) const
+{
+    auto it = _objects.find(id);
+    if (it != _objects.end()) {
+        return it->second;
+    }
+    //raise Eroor Object not found
+    return nullptr;
+}
+
+void ObjectManager::clearObjects()
+{
+    _objects.clear();
+}
+
+void ObjectManager::duplicateObject(const long& id)
+{
+    auto it = _objects.find(id);
+    if (it != _objects.end()) {
+        long newId = id + 1;
+
+        while (_objects.find(newId) != _objects.end()) {
+            newId++;
+        }
+
+        _objects[newId] = it->second;
+    } else {
+        //raise Eroor Object not found
+    }
+}
---
+++ src/engine/SceneManager.cpp
@@ -0,0 +1,107 @@
+/*
+** EPITECH PROJECT, 2024
+** StellarForge
+** File description:
+** SceneManager.cpp
+*/
+
+#include "SceneManager.hpp"
+
+void SceneManager::addScene(const long &id, std::shared_ptr<void *> scene, int position = -1)
+{
+    if (_scenes.find(id) != _scenes.end()) {
+            //raise Warning ID already exists, does not do anything
+            return;
+        }
+        _scenes[id] = scene;
+
+        if (position < 0 || position >= _sceneOrder.size()) {
+            _sceneOrder.push_back(id);
+        } else {
+            _sceneOrder.insert(_sceneOrder.begin() + position, id);
+        }
+
+        if (_currentSceneIndex == -1) {
+            _currentSceneIndex = 0;
+        }
+}
+
+void SceneManager::removeScene(const long& id) {
+    auto it = std::find(_sceneOrder.begin(), _sceneOrder.end(), id);
+    if (it != _sceneOrder.end()) {
+        _sceneOrder.erase(it);
+
+        if (_sceneOrder.empty()) {
+            _currentSceneIndex = -1;
+        } else if (_currentSceneIndex >= _sceneOrder.size()) {
+            _currentSceneIndex = _sceneOrder.size() - 1;
+        }
+    }
+
+    _scenes.erase(id);
+}
+
+void SceneManager::switchToScene(const long& id) {
+    auto it = std::find(_sceneOrder.begin(), _sceneOrder.end(), id);
+    if (it != _sceneOrder.end()) {
+        _currentSceneIndex = std::distance(_sceneOrder.begin(), it);
+    } else {
+        //raise Warning ID does not exist
+    }
+}
+
+void SceneManager::switchToNextScene() {
+    if (_currentSceneIndex >= 0 && _currentSceneIndex < _sceneOrder.size() - 1) {
+        _currentSceneIndex++;
+    } else {
+        //raise Warning No next scene
+    }
+}
+
+void SceneManager::switchToPreviousScene() {
+    if (_currentSceneIndex > 0) {
+        _currentSceneIndex--;
+    } else {
+        //raise Warning No previous scene
+    }
+}
+
+void SceneManager::switchScenesOrder(const long& id1, const long& id2) {
+    auto it1 = std::find(_sceneOrder.begin(), _sceneOrder.end(), id1);
+    auto it2 = std::find(_sceneOrder.begin(), _sceneOrder.end(), id2);
+
+    if (it1 != _sceneOrder.end() && it2 != _sceneOrder.end()) {
+        std::iter_swap(it1, it2);
+    } else {
+        //raise Error Scene not found
+    }
+}
+
+std::shared_ptr<void*> SceneManager::getCurrentScene() const {
+    if (_currentSceneIndex >= 0 && _currentSceneIndex < _sceneOrder.size()) {
+        return _scenes.at(_sceneOrder[_currentSceneIndex]);
+    }
+    return nullptr;
+}
+
+std::shared_ptr<void*> SceneManager::getSceneById(const long& id) const {
+    auto it = _scenes.find(id);
+    if (it != _scenes.end()) {
+        return it->second;
+    }
+    return nullptr;
+}
+
+std::map<long, std::shared_ptr<void*>> SceneManager::getScenes() const {
+    return _scenes;
+}
+
+std::vector<long> SceneManager::getSceneOrder() const {
+    return _sceneOrder;
+}
+
+void SceneManager::clearScenes() {
+    _scenes.clear();
+    _sceneOrder.clear();
+    _currentSceneIndex = -1;
+}

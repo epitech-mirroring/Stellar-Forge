64d494dfc0eaa7e338077f804295e4d9290db3fb
Marius-P1 <marius.pain@epitech.eu>
2024-09-24 16:53:58 +0200

    :sparkles: Add the EventManager class to contain and process events

---
+++ src/common/event/EventManager.hpp
@@ -0,0 +1,81 @@
+/*
+** EPITECH PROJECT, 2024
+** Stellar-Forge
+** File description:
+** EventManager
+*/
+
+#pragma once
+
+#include "Event.hpp"
+
+#include <unordered_map>
+
+/**
+ * @brief Event manager class
+ *
+ * @note This class is used to manage events
+ */
+class EventManager {
+public:
+    /**
+     * @brief Construct a new Event Manager object
+     *
+     * @note This constructor creates a new event manager
+     */
+    EventManager();
+
+    /**
+     * @brief Destroy the Event Manager object
+     *
+     * @note This destructor destroys the event manager
+     */
+    ~EventManager();
+
+    /**
+     * @brief Add an event to the event manager
+     *
+     * @param name std::string, name of the event
+     * @note This function adds an event to the event manager
+     */
+    void AddEvent(std::string name);
+
+    /**
+     * @brief Remove an event from the event manager
+     *
+     * @param name std::string, name of the event
+     * @note This function removes an event from the event manager
+     */
+    void RemoveEvent(std::string name);
+
+    /**
+     * @brief Add a consumer to an event
+     *
+     * @param name std::string, name of the event
+     * @param consumer EventConsumer, consumer to add
+     * @note This function adds a consumer to an event
+     */
+    void AddConsumerToEvent(std::string name, EventConsumer consumer);
+
+    /**
+     * @brief Remove a consumer from an event
+     *
+     * @param name std::string, name of the event
+     * @param consumer EventConsumer, consumer to remove
+     * @note This function removes a consumer from an event
+     */
+    void RemoveConsumerFromEvent(std::string name, EventConsumer consumer);
+
+    /**
+     * @brief Call all consumers of an event
+     *
+     * @param name std::string, name of the event
+     * @note This function calls all consumers of an event
+     */
+    void CallConsumersFromEvent(std::string name);
+
+protected:
+private:
+    // Events map (name -> event)
+    std::unordered_map<std::string, Event> _events;
+};

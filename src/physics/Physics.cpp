801e1671f93772a487f153546ce508b066ef9b81
landryarki <landry.gigant@epitech.eu>
2024-09-25 17:56:06 +0200

    :sparkles: collision Boxs and Spheres
    BoxVsBox
    BoxVsSphere
    SphereVsBox
    SphereVsSphere

--- src/physics/Physics.cpp
+++ src/physics/Physics.cpp
@@ -8,4 +8,30 @@
 
 #include "Physics.hpp"
 
-Physics::Physics() = default;
+bool Physics::Collision::BoxVsBox(Box box1, Box box2) {
+    return (box1.position.x < box2.position.x + box2.size.x &&
+            box1.position.x + box1.size.x > box2.position.x &&
+            box1.position.y < box2.position.y + box2.size.y &&
+            box1.position.y + box1.size.y > box2.position.y &&
+            box1.position.z < box2.position.z + box2.size.z &&
+            box1.position.z + box1.size.z > box2.position.z);
+}
+
+bool Physics::Collision::SphereVsSphere(Sphere sphere1, Sphere sphere2) {
+    return glm::distance(sphere1.position, sphere2.position) < sphere1.radius + sphere2.radius;
+}
+
+bool Physics::Collision::BoxVsSphere(Box box, Sphere sphere) {
+    Vector3 closestPoint = glm::vec3(
+            glm::clamp(sphere.position.x, std::min(box.position.x, box.position.x + box.size.x),
+                       std::max(box.position.x, box.position.x + box.size.x)),
+            glm::clamp(sphere.position.y, std::min(box.position.y, box.position.y + box.size.y),
+                       std::max(box.position.y, box.position.y + box.size.y)),
+            glm::clamp(sphere.position.z, std::min(box.position.z, box.position.z + box.size.z),
+                       std::max(box.position.z, box.position.z + box.size.z))
+    );
+    return glm::distance(sphere.position, closestPoint) < sphere.radius;
+}
+bool Physics::Collision::SphereVsBox(Sphere sphere, Box box) {
+    return BoxVsSphere(box, sphere);
+}
--- src/physics/Physics.hpp
+++ src/physics/Physics.hpp
@@ -11,9 +11,38 @@
 
 
 
+#include "glm/glm.hpp"
+
 class Physics {
 public:
-    Physics();
+
+    using Vector3 = glm::vec3;
+
+    class Box {
+    public:
+        Vector3 position;
+        Vector3 size;
+
+        Box(Vector3 position, Vector3 size) : position(position), size(size) {};
+        ~Box() = default;
+    };
+
+    class Sphere {
+    public:
+        Vector3 position;
+        float radius;
+
+        Sphere(Vector3 position, float radius) : position(position), radius(radius) {};
+        ~Sphere() = default;;
+    };
+
+    class Collision {
+    public:
+        static bool BoxVsBox(Box box1, Box box2);
+        static bool SphereVsSphere(Sphere sphere1, Sphere sphere2);
+        static bool BoxVsSphere(Box box, Sphere sphere);
+        static bool SphereVsBox(Sphere sphere, Box box);
+    };
 };
 
 

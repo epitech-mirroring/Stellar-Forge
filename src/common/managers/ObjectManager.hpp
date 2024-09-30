efb6522d4ba75b183c11eae40fea15ee79e3129a
Aubane <aubane.nourry@epitech.eu>
2024-09-30 23:09:28 +0200

    :memo: Add managers documentation in headers

--- src/common/managers/ObjectManager.hpp
+++ src/common/managers/ObjectManager.hpp
@@ -14,23 +14,134 @@
 #include "../UUID.hpp"
 #include "ManagerException.hpp"
 
+/**
+ * @class ObjectManager
+ * @brief Manages objects in the StellarForge engine.
+ * @details This class provides an interface for managing multiple objects, including adding, removing, and updating them.
+ * @version v0.1.0
+ * @since v0.1.0
+ * @author Aubane NOURRY
+ */
 class ObjectManager {
 public:
+    /**
+     * @brief Default constructor for ObjectManager.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     ObjectManager() = default;
+
+    /**
+     * @brief Destructor for ObjectManager.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     ~ObjectManager() = default;
 
+    /**
+     * @brief Retrieves all objects managed by the ObjectManager.
+     * @return An unordered map of UUIDs to object pointers.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     std::unordered_map<UUID, IObject *> getObjects() const;
+
+    /**
+     * @brief Gets the total count of objects.
+     * @return The number of objects managed by the ObjectManager.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     size_t getObjectCount() const;
+
+    /**
+     * @brief Checks if an object exists in the manager.
+     * @param id The unique identifier of the object.
+     * @return True if the object exists, false otherwise.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     bool objectExists(const UUID& id) const;
+
+    /**
+     * @brief Adds an object to the manager.
+     * @param id The unique identifier of the object.
+     * @param object The object to be added.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     void addObject(const UUID& id, IObject *object);
+
+    /**
+     * @brief Adds multiple objects to the manager.
+     * @param objects An unordered map of UUIDs to object pointers.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     void addObjects(const std::unordered_map<UUID, IObject *>& objects);
+
+    /**
+     * @brief Removes an object from the manager.
+     * @param id The unique identifier of the object to remove.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     void removeObject(const UUID& id);
+
+    /**
+     * @brief Removes multiple objects from the manager.
+     * @param ids A vector of UUIDs representing the objects to remove.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     void removeObjects(const std::vector<UUID>& ids);
+
+    /**
+     * @brief Updates an object in the manager.
+     * @param id The unique identifier of the object.
+     * @param updatedObject The updated object.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     void updateObject(const UUID& id, IObject *updatedObject);
+
+    /**
+     * @brief Retrieves an object by its unique identifier.
+     * @param id The unique identifier of the object.
+     * @return A pointer to the object.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     IObject *getObjectById(const UUID& id) const;
+
+    /**
+     * @brief Clears all objects managed by the ObjectManager.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     void clearObjects();
+
+    /**
+     * @brief Duplicates an object within the manager.
+     * @param id The unique identifier of the object to duplicate.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     void duplicateObject(const UUID& id);
-    
+
 private:
-    std::unordered_map<UUID, IObject *> _objects;
+    std::unordered_map<UUID, IObject *> _objects;  ///< Unordered map of UUIDs to objects.
 };
--- src/common/managers/SceneManager.hpp
+++ src/common/managers/SceneManager.hpp
@@ -15,25 +15,136 @@
 #include "../UUID.hpp"
 #include "ManagerException.hpp"
 
+/**
+ * @class SceneManager
+ * @brief Manages scenes in the StellarForge engine.
+ * @details This class provides an interface for managing multiple scenes in the application. It allows adding, removing, and switching between scenes.
+ * @version v0.1.0
+ * @since v0.1.0
+ * @author Aubane NOURRY
+ */
 class SceneManager {
 public:
+    /**
+     * @brief Default constructor for SceneManager.
+     * @details Initializes the SceneManager with no scenes and sets the current scene index to -1.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     SceneManager() : _currentSceneIndex(-1) {}
+
+    /**
+     * @brief Destructor for SceneManager.
+     * @details Cleans up the SceneManager when it is no longer in use.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     ~SceneManager() = default;
 
+    /**
+     * @brief Adds a scene to the manager.
+     * @param id The unique identifier of the scene.
+     * @param scene A shared pointer to the scene to be added.
+     * @param position The position to add the scene at. Default is -1, which appends the scene.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     void addScene(const UUID& id, std::shared_ptr<IScene> scene, int position /*default -1*/);
+
+    /**
+     * @brief Removes a scene from the manager.
+     * @param id The unique identifier of the scene to be removed.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     void removeScene(const UUID& id);
+
+    /**
+     * @brief Switches to a specific scene.
+     * @param id The unique identifier of the scene to switch to.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     void switchToScene(const UUID& id);
+
+    /**
+     * @brief Switches to the next scene in the order.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     void switchToNextScene();
+
+    /**
+     * @brief Switches to the previous scene in the order.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     void switchToPreviousScene();
+
+    /**
+     * @brief Switches the order of two scenes.
+     * @param id1 The unique identifier of the first scene.
+     * @param id2 The unique identifier of the second scene.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     void switchScenesOrder(const UUID& id1, const UUID& id2);
+
+    /**
+     * @brief Gets the current scene.
+     * @return A shared pointer to the current scene.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     std::shared_ptr<IScene> getCurrentScene() const;
+
+    /**
+     * @brief Retrieves a scene by its unique identifier.
+     * @param id The unique identifier of the scene.
+     * @return A shared pointer to the scene.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     std::shared_ptr<IScene> getSceneById(const UUID& id) const;
+
+    /**
+     * @brief Clears all scenes managed by the SceneManager.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     void clearScenes();
+
+    /**
+     * @brief Retrieves all the scenes managed by the SceneManager.
+     * @return A map of UUIDs and shared pointers to the scenes.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     std::map<UUID, std::shared_ptr<IScene>> getScenes() const;
+
+    /**
+     * @brief Retrieves the order of the scenes.
+     * @return A vector of UUIDs representing the order of the scenes.
+     * @version v0.1.0
+     * @since v0.1.0
+     * @author Aubane NOURRY
+     */
     std::vector<UUID> getSceneOrder() const;
 
 private:
-    std::map<UUID, std::shared_ptr<IScene>> _scenes;
-    std::vector<UUID> _sceneOrder;
-    int _currentSceneIndex;
+    std::map<UUID, std::shared_ptr<IScene>> _scenes;  ///< Map of UUIDs to scenes.
+    std::vector<UUID> _sceneOrder;  ///< Order of the scenes.
+    int _currentSceneIndex;  ///< The index of the current scene.
 };

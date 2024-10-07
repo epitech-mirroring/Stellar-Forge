17ff2839772e3e025be6f1f298d6f182c3eb20c0
Aubane <aubane.nourry@epitech.eu>
2024-10-07 10:58:17 +0200

    :building_construction: Switch from SDL2 to SFML

--- src/graphics/Graphics.cpp
+++ src/graphics/Graphics.cpp
@@ -8,4 +8,8 @@
 
 #include "Graphics.hpp"
 
-Graphics::Graphics() = default;
+Graphics::Graphics(int width, int height, const char *title, bool precharge)
+    : width(width), height(height), title(title), precharge(precharge), initialized(false), currentScene(nullptr)
+{
+    window.create(sf::VideoMode(width, height), title);
+}

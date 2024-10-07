e3bacba1c7267f5cbe2d0b2a8eb4f6b4feb348e7
Aubane <aubane.nourry@epitech.eu>
2024-10-07 14:08:30 +0200

    :construction: WIP SFML lib include

--- src/graphics/Graphics.cpp
+++ src/graphics/Graphics.cpp
@@ -8,11 +8,9 @@
 
 #include "Graphics.hpp"
 
-Graphics::Graphics(int width, int height, const char *title, bool precharge) {
-    this->width = width;
-    this->height = height;
-    this->title = title;
-    this->precharge = precharge;
-    this->window.create(sf::VideoMode(width, height), title);
+Graphics::Graphics(int width, int height, const char *title, bool precharge)
+    : width(width), height(height), title(title), precharge(precharge),
+    window(sf::VideoMode(width, height), title), prepared(false), currentScene(nullptr)
+{
     this->window.setFramerateLimit(60);
 }

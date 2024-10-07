1a9aaeeeea59a089fb023f502c1387d72fc3734e
Aubane <aubane.nourry@epitech.eu>
2024-10-07 18:34:28 +0200

    :sparkles: Add internal clock to AnimatedSprite

--- src/graphics/components/AnimatedSprite.hpp
+++ src/graphics/components/AnimatedSprite.hpp
@@ -13,13 +13,13 @@
 class AnimatedSprite : public AGraphicsComponent {
 public:
     AnimatedSprite(IObject* owner, const char *path, std::vector<sf::IntRect> frames, float frameTime = 0.1f, int frame = 0);
-    ~AnimatedSprite() override;
+    ~AnimatedSprite() = default;
 
     void render(sf::RenderWindow *window) override;
+    void runComponent() override;
     void setTexture(const char *path);
     void setFrames(std::vector<sf::IntRect> frames);
-    void setFrame(sf::IntRect frame);
-    void setFrame(int frame);
+    void setFrame(unsigned int frame);
     void nextFrame();
     void prevFrame();
     void setFrameTime(float frameTime);
@@ -36,6 +36,8 @@ private:
     std::vector<sf::IntRect> frames;
     int currentFrame;
     float frameTime;
+    sf::Clock clock;
+    const char *path;
 };
 
 #endif //ANIMATEDSPRITE_HPP

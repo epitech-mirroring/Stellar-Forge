2b332c7954b4df494cefad0aa34fe4edb9a57859
Axel Eckenberg <axel.eckenberg@yahoo.fr>
2024-10-13 00:04:49 +0200

    🐛 Fix windows demangler

--- src/common/Utils.hpp
+++ src/common/Utils.hpp
@@ -8,11 +8,36 @@
 
 #ifndef UTILS_HPP
 #define UTILS_HPP
+#if defined(__linux__) || defined(__APPLE__)
 #include <cxxabi.h>
 
 template<typename T>
 std::string get_typename([[maybe_unused]] T *object) {
     return abi::__cxa_demangle(typeid(*object).name(), nullptr, nullptr, nullptr);
 }
+
+#else
+#if defined(_WIN32)
+
+#include <dbghelp.h>
+
+template<typename T>
+auto get_typename() {
+    auto type_info = typeid(T);
+    char *name = nullptr;
+    auto status = UnDecorateSymbolName(type_info.name(), name, 0, 0);
+    if (status == 0) {
+        return std::string(type_info.name());
+    }
+    return std::string(name);
+}
+#endif
+
+template<typename T>
+std::string get_typename([[maybe_unused]] T *object) {
+    return typeid(*object).name();
+}
+
+#endif
 
 #endif //UTILS_HPP

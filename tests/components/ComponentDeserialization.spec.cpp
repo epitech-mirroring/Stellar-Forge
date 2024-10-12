e4e3085145bb28e915a337d2782ec823fa132139
Axel Eckenberg <axel.eckenberg@yahoo.fr>
2024-10-12 17:54:31 +0200

    ✅ Add component deserialization test
    This test uses the component factory so in a way it’s also tested

---
+++ tests/components/ComponentDeserialization.spec.cpp
@@ -0,0 +1,58 @@
+/*
+** EPITECH PROJECT, 2024
+** StellarForge
+** File description:
+** No file there, just an epitech header example.
+** You can even have multiple lines if you want!
+*/
+
+#include <gtest/gtest.h>
+
+#include "common/components/Transform.hpp"
+#include "common/json/JsonArray.hpp"
+#include "common/json/JsonParser.hpp"
+
+TEST(ComponentDeserialization, Transform) {
+    std::string const json = R"({
+        "type": "Transform",
+        "isActive": true,
+        "data": {
+            "position": {
+                "x": 1,
+                "y": 2,
+                "z": 3
+            },
+            "rotation": {
+                "x": 4,
+                "y": 5,
+                "z": 6
+            },
+            "scale": {
+                "x": 7,
+                "y": 8,
+                "z": 9
+            }
+        }
+    })";
+
+    REGISTER_COMPONENT(Transform);
+    json::JsonParser const parser;
+    const auto *const raw = parser.parse(json);
+    ASSERT_EQ(raw->getType(), json::OBJECT);
+    ASSERT_EQ(raw->getName(), "root");
+    const auto *const data = dynamic_cast<const json::JsonObject *>(raw);
+    IComponent *r_component = ComponentFactory::create("Transform", nullptr, data);
+    ASSERT_NE(r_component, nullptr);
+    ASSERT_EQ(r_component->isActive(), true);
+    ASSERT_NE(dynamic_cast<Transform *>(r_component), nullptr);
+    const Transform *transform = dynamic_cast<Transform *>(r_component);
+    ASSERT_FLOAT_EQ(transform->getPosition().x, 1);
+    ASSERT_FLOAT_EQ(transform->getPosition().y, 2);
+    ASSERT_FLOAT_EQ(transform->getPosition().z, 3);
+    ASSERT_FLOAT_EQ(transform->getRotation().x, 4);
+    ASSERT_FLOAT_EQ(transform->getRotation().y, 5);
+    ASSERT_FLOAT_EQ(transform->getRotation().z, 6);
+    ASSERT_FLOAT_EQ(transform->getScale().x, 7);
+    ASSERT_FLOAT_EQ(transform->getScale().y, 8);
+    ASSERT_FLOAT_EQ(transform->getScale().z, 9);
+}

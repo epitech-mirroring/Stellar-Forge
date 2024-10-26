2fe412d48d2bd194ce1d6db98e0fe4c062370a07
Axel Eckenberg <axel.eckenberg@yahoo.fr>
2024-10-26 12:16:25 +0200

    🎨 Add missing indentation

--- src/common/components/AComponent.hpp
+++ src/common/components/AComponent.hpp
@@ -101,7 +101,7 @@ public:
  [[nodiscard]] const IMeta &getMeta() const override;
 
  template<typename T>
-T *getParentComponent() {
+ T *getParentComponent() {
   static_assert(std::is_base_of_v<IComponent, T>, "T must inherit from IComponent");
   for (auto &component: this->_owner->getComponents()) {
    if (dynamic_cast<T *>(component) != nullptr) {

d08431422428eca74c612c8a7c92db9558960f6a
6im0n <simon.gl3135@orange.fr>
2024-11-03 07:20:17 +0100

    :building_construction: Add installation of tools and build for 3D engine

--- build-linux.sh
+++ build-linux.sh
@@ -1,59 +1,120 @@
 #!/bin/bash
 
-# Set script to exit on any errors.
-set -e
-
 # Variables for dependency directories
 BUILD_DIR="build"
 CONAN_PROFILE="default"
 
-# Function to output information messages in yellow
-function write_info {
-    local message="$1"
-    echo -e "\e[33m[Info] $message\e[0m"
+# Check if the script is run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root. Please run with sudo or as root user."
+   exit 1
+fi
+
+#Get the os type
+os=$(uname -a)
+os_type=""
+
+
+# Function to print info messages in yellow color
+info() {
+  echo -e "\033[1;33m[info] $1\033[0m"
 }
 
-# Check if a command exists, install if not
-function check_and_install {
-    local cmd="$1"
-    local pkg="$2"
-    write_info "Checking if $cmd is installed..."
-    if ! command -v "$cmd" &> /dev/null; then
-        write_info "$cmd not found. Installing $pkg..."
-        sudo dnf install -y "$pkg"
-        write_info "$cmd installed."
-    else
-        write_info "$cmd is already installed."
+# Step: Check if python is installed
+info "Checking if python is installed..."
+if ! command -v python &> /dev/null
+then
+    info "Python not found, installing Python..."
+    $package_manager install -y python
+    if [ $os_type == "debian" ]; then
+        $package_manager install -y python3-full
     fi
-}
+fi
+info "Python is installed."
 
-# Install dependencies
-write_info "Installing required packages..."
+# Step: Check if pip is installed
+info "Checking if pip is installed..."
+if ! command -v pip &> /dev/null
+then
+    info "Pip not found, installing Pip..."
+    $package_manager install -y python-pip
+    if [ $os_type == "debian" ]; then
+        $package_manager install -y python3-pip
+    fi
+fi
+info "Pip is installed."
 
-# Conan
-check_and_install "conan" "conan"
+# for debian based systems check if pkg-config is installed
+if [ $os_type == "debian" ]; then
+    info "Checking if pkg-config is installed..."
+    if ! command -v pkg-config &> /dev/null
+    then
+        info "Pkg-config not found, installing Pkg-config..."
+        $package_manager install -y pkg-config
+    fi
+fi
 
-# CMake
-check_and_install "cmake" "cmake"
+# for fedora based systems check if build-essential is installed
+if [ $os_type == "fedora" ]; then
+    info "Checking if build-essential is installed..."
+    dnf install -y make automake gcc gcc-c++ kernel-devel
+fi
 
-# Ninja (for build tools)
-check_and_install "ninja" "ninja-build"
+# for debian based systems check if build-essential is installed
+if [ $os_type == "debian" ]; then
+    info "Checking if build-essential is installed..."
+    if ! command -v build-essential &> /dev/null
+    then
+        info "Build-essential not found, installing Build-essential..."
+        $package_manager install -y build-essential
+    fi
+fi
 
-# Python3 (includes pip)
-check_and_install "pip3" "python3-pip"
+# Install Conan if not already installed
+info "Checking if Conan is installed..."
+if ! command -v conan &> /dev/null
+then
+    info "Conan not found, installing Conan..."
+    if [ $os_type == "fedora" ]; then
+        pip install conan
+    fi
+    if [ $os_type == "debian" ]; then
+        pip install conan --break-system-packages
+    fi
+fi
 
-# C++ compiler (gcc-c++)
-check_and_install "g++" "gcc-c++"
+# Install CMake if not already installed
+info "Checking if CMake is installed..."
+if ! command -v cmake &> /dev/null
+then
+    info "CMake not found, installing CMake..."
+    $package_manager install -y cmake
+fi
 
-write_info "All required packages are installed."
+# Install Ninja if not already installed
+info "Checking if Ninja is installed..."
+if ! command -v ninja &> /dev/null
+then
+    info "Ninja not found, installing Ninja..."
+    $package_manager install -y ninja-build
+fi
 
+# Install C++ compiler if not already installed
+info "Checking if C++ compiler is installed..."
+if ! command -v g++ &> /dev/null
+then
+    info "C++ compiler not found, installing C++ compiler..."
+    $package_manager install -y gcc-c++
+fi
+
+info "Removing Conan cache..."
 conan remote remove Epitech-Mirroring
 conan remove '*' -c
 conan cache clean '*'
 conan profile detect --force
 
-
 # Common
+info "Create Conan packages for common library..."
 cd ./src/common && \
 mkdir -p "build" && \
 cd "build" && \
@@ -64,6 +125,7 @@ conan create . -s:a build_type=Debug --build=missing &&
 cd ../../
 
 # Physics
+info "Create Conan packages for physics library..."
 cd ./src/physics && \
 mkdir -p "build" && \
 cd "build" && \
@@ -74,6 +136,7 @@ conan create . -s:a build_type=Debug --build=missing &&
 cd ../../
 
 # Graphics
+info "Create Conan packages for graphics library..."
 cd ./src/graphics && \
 mkdir -p "build" && \
 cd "build" && \
@@ -84,7 +147,30 @@ conan create . -s:a build_type=Debug --build=missing &&
 cd ../../
 
 # Engine
+info "Create Conan packages for engine library..."
 cd ./src/engine && \
+mkdir -p "build" && \
+cd "build" && \
+conan install .. --build=missing && \
+cd .. && \
+conan create . --build=missing && \
+conan create . -s:a build_type=Debug --build=missing &&
+cd ../../
+
+# Graphics3D
+info "Create Conan packages for graphics library..."
+cd ./src/graphics3D && \
+mkdir -p "build" && \
+cd "build" && \
+conan install .. --build=missing && \
+cd .. && \
+conan create . --build=missing && \
+conan create . -s:a build_type=Debug --build=missing &&
+cd ../../
+
+# Engine3D
+info "Create Conan packages for engine library..."
+cd ./src/engine3D && \
 mkdir -p "build" && \
 cd "build" && \
 conan install .. --build=missing && \
@@ -101,10 +187,6 @@ if [ ! -d "$BUILD_DIR" ]; then
   mkdir -p "$BUILD_DIR"
 fi
 
-# Run Conan to install missing dependencies
-info "Changing to build directory..."
-cd "$BUILD_DIR"
-
 info "Ensuring Conan profile is available..."
 if ! conan profile list | grep -q "$CONAN_PROFILE"; then
     info "Creating Conan profile: $CONAN_PROFILE"
@@ -112,12 +194,16 @@ if ! conan profile list | grep -q "$CONAN_PROFILE"; then
 fi
 
 info "Installing dependencies with Conan..."
-conan install .. --build=missing --profile="$CONAN_PROFILE" -g CMakeDeps -g CMakeToolchain
+conan install . --output-folder="build" --build=missing -s:a build_type=Release -c tools.system.package_manager:mode=install
 
 # Run CMake to configure the build
 info "Running CMake to configure the build..."
-cmake -S .. -B . -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_PREFIX_PATH="." -DCMAKE_BUILD_TYPE=Release
+cmake -B ./build -DCMAKE_TOOLCHAIN_FILE="build/build/Release/generators/conan_toolchain.cmake" -DCMAKE_PREFIX_PATH="build" -DCMAKE_BUILD_TYPE=Release
+
+# Step: Detect number of available threads for parallel build
+NUM_THREADS=$(nproc --all)
+info "Number of threads available for build: $NUM_THREADS"
 
 # Build the project with CMake
 info "Building the project with CMake..."
-cmake --build . --target StellarForge
+cmake --build build --target StellarForge -- -j "$NUM_THREADS"
